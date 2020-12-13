a25138e48cd54c037239b8537a781a9640cfce63
unknown <you@example.com>
2020-12-13 23:54:47 +0530

    patint_Rgistr_BookAppointmnt

---
+++ hospitalsystem/hmps/patient/register.php
@@ -0,0 +1,125 @@
+<?php
+include_once('include/config.php');
+if(isset($_POST['submit']))
+{
+$fname=$_POST['full_name'];
+$address=$_POST['address'];
+$city=$_POST['city'];
+$gender=$_POST['gender'];
+$email=$_POST['email'];
+$password=md5($_POST['password']);
+$query=$conn->query("insert into patient(fullname,address,city,gender,email,password) values('$fname','$address','$city','$gender','$email','$password')");
+if($query)
+{
+	echo "<script>alert('Successfully Registered. You can login now');</script>";
+	//header('location:user-login.php');
+}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Kodinger">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>MediCare</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/my-login.css">
+
+	<!-- Favicons -->
+  <link href="assets/img/favicon.jpg" rel="icon">
+  <link href="assets/img/apple-touch-icon.jpg" rel="apple-touch-icon">
+</head>
+<body class="my-login-page ">
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-md-center h-100">
+				<div class="card-wrapper">
+					<div class="brand">
+						<img src="img/medicare.jpg" alt="logo">
+					</div>
+					<div class="card fat">
+						<div class="card-body">
+							<h4 class="card-title">Register</h4>
+							<form method="POST" class="my-login-validation" novalidate="">
+								<p>
+								Enter your personal details below:
+							</p>
+							<div class="form-group">
+								<input type="text" class="form-control" name="full_name" placeholder="Full Name" required>
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control" name="address" placeholder="Address" required>
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control" name="city" placeholder="City" required>
+							</div>
+							<div class="form-group">
+								<label class="block">
+									Gender
+								</label>
+								<div class="clip-radio radio-primary">
+									<input type="radio" id="rg-female" name="gender" value="female" >
+									<label for="rg-female">
+										Female
+									</label>
+									<input type="radio" id="rg-male" name="gender" value="male">
+									<label for="rg-male">
+										Male
+									</label>
+								</div>
+							</div>
+							<p>
+								Enter your account details below:
+							</p>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="email" class="form-control" name="email" id="email" onBlur="userAvailability()"  placeholder="Email" required>
+									<i class="fa fa-envelope"></i> </span>
+									 <span id="user-availability-status1" style="font-size:12px;"></span>
+							</div>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+									<i class="fa fa-lock"></i> </span>
+							</div>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="password" class="form-control" name="password_again" placeholder="Password Again" required>
+									<i class="fa fa-lock"></i> </span>
+							</div>
+								<div class="form-group">
+									<div class="custom-checkbox custom-control">
+										<input type="checkbox" name="agree" id="agree" class="custom-control-input" required="">
+										<label for="agree" class="custom-control-label">I agree to the <a href="#">Terms and Conditions</a></label>
+										<div class="invalid-feedback">
+											You must agree with our Terms and Conditions
+										</div>
+									</div>
+								</div>
+
+								<div class="form-group m-0">
+									<button  name="submit"  id="submit" type="submit" class="btn btn-primary btn-block">
+										Register
+									</button>
+								</div>
+								<div class="mt-4 text-center">
+									Already have an account? <a href="index.php">Login</a>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="footer">
+						MediCare we care
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
